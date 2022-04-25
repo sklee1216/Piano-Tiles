4a4d9697c4de9b74be4c295d1773ca3e04c098c3
sklee1216 <sglee980708@gmail.com>
2022-04-25 17:11:18 -0400

    update

--- support.c
+++ support.c
@@ -14,22 +14,23 @@ struct{
     int x2;
     int y2;
     int flag;
-} tiles[] = {{0,-20,30,0,0},
-        {30,-20,60,0,0},
-        {60,-20,90,0,0},
-        {90,-20,120,0,0},
-        {120,-20,150,0,0},
-        {150,-20,180,0,0},
-        {180,-20,210,0,0},
-        {210,-20,240,0,0},
-        {0,-20,30,0,0},
-                {30,-20,60,0,0},
-                {60,-20,90,0,0},
-                {90,-20,120,0,0},
-                {120,-20,150,0,0},
-                {150,-20,180,0,0},
-                {180,-20,210,0,0},
-                {210,-20,240,0,0}
+    int color;
+} tiles[] = {{1,-20,29,0,0,WHITE},
+        {31,-20,59,0,0,WHITE},
+        {61,-20,89,0,0,WHITE},
+        {91,-20,119,0,0,WHITE},
+        {121,-20,149,0,0,WHITE},
+        {151,-20,179,0,0,WHITE},
+        {181,-20,209,0,0,WHITE},
+        {211,-20,239,0,0,WHITE},
+        {1,-20,29,0,0,WHITE},
+        {31,-20,59,0,0,WHITE},
+        {61,-20,89,0,0,WHITE},
+        {91,-20,119,0,0,WHITE},
+        {121,-20,149,0,0,WHITE},
+        {151,-20,179,0,0,WHITE},
+        {181,-20,209,0,0,WHITE},
+        {211,-20,239,0,0,WHITE},
 };
 
 
@@ -51,91 +52,50 @@ void nano_wait(unsigned int n) {
 }
 
 
-// Copy a subset of a large source picture into a smaller destination.
-// sx,sy are the offset into the source picture.
-void pic_subset(Picture *dst, const Picture *src, int sx, int sy)
-{
-    int dw = dst->width;
-    int dh = dst->height;
-    for(int y=0; y<dh; y++) {
-        if (y+sy < 0)
-            continue;
-        if (y+sy >= src->height)
-            break;
-        for(int x=0; x<dw; x++) {
-            if (x+sx < 0)
-                continue;
-            if (x+sx >= src->width)
-                break;
-            dst->pix2[dw * y + x] = src->pix2[src->width * (y+sy) + x + sx];
-        }
-    }
-}
-
-
-// Overlay a picture onto a destination picture.
-// xoffset,yoffset are the offset into the destination picture that the
-// source picture is placed.
-// Any pixel in the source that is the 'transparent' color will not be
-// copied.  This defines a color in the source that can be used as a
-// transparent color.
-void pic_overlay(Picture *dst, int xoffset, int yoffset, const Picture *src, int transparent)
-{
-    for(int y=0; y<src->height; y++) {
-        int dy = y+yoffset;
-        if (dy < 0)
-            continue;
-        if (dy >= dst->height)
-            break;
-        for(int x=0; x<src->width; x++) {
-            int dx = x+xoffset;
-            if (dx < 0)
-                continue;
-            if (dx >= dst->width)
-                break;
-            unsigned short int p = src->pix2[y*src->width + x];
-            if (p != transparent)
-                dst->pix2[dy*dst->width + dx] = p;
-        }
-    }
-}
-
 extern const Picture background; // A 240x320 background image
-extern const Picture ball; // A 19x19 purple ball with white boundaries
 
-// This C macro will create an array of Picture elements.
-// Really, you'll just use it as a pointer to a single Picture
-// element with an internal pix2[] array large enough to hold
-// an image of the specified size.
-// BE CAREFUL HOW LARGE OF A PICTURE YOU TRY TO CREATE:
-// A 100x100 picture uses 20000 bytes.  You have 32768 bytes of SRAM.
-#define TempPicturePtr(name,width,height) Picture name[(width)*(height)/6+2] = { {width,height,2} }
-
-void erase(int x, int y)
-{
-    TempPicturePtr(tmp,29,29); // Create a temporary 29x29 image.
-    pic_subset(tmp, &background, x-tmp->width/2, y-tmp->height/2); // Copy the background
-    //pic_overlay(tmp, 5,5, &ball, 0xffff); // Overlay the ball
-    LCD_DrawPicture(x-tmp->width/2,y-tmp->height/2, tmp); // Draw
-}
-
-void update2(int x, int y)
-{
-    TempPicturePtr(tmp,29,29); // Create a temporary 29x29 image.
-    pic_subset(tmp, &background, x-tmp->width/2, y-tmp->height/2); // Copy the background
-    pic_overlay(tmp, 5,5, &ball, 0xffff); // Overlay the ball
-    LCD_DrawPicture(x-tmp->width/2,y-tmp->height/2, tmp); // Draw
-}
 
 void move_tiles(int tile_num){
 
     if (tiles[tile_num].flag == 1){
+           LCD_DrawFillRectangle(tiles[tile_num].x1, tiles[tile_num].y1, tiles[tile_num].x2, tiles[tile_num].y1+1, BLACK);
 
-    LCD_DrawFillRectangle(tiles[tile_num].x1, tiles[tile_num].y2 +1, tiles[tile_num].x2, tiles[tile_num].y2 + 2,WHITE);
-    LCD_DrawFillRectangle(tiles[tile_num].x1, tiles[tile_num].y1, tiles[tile_num].x2, tiles[tile_num].y1+1, BLACK);
+           if(tiles[tile_num].x1 == 1){
+               LCD_DrawFillRectangle(tiles[tile_num].x1, tiles[tile_num].y2 +1, tiles[tile_num].x2, tiles[tile_num].y2 + 2, RED);
+           }
+           else if(tiles[tile_num].x1 == 31){
+               LCD_DrawFillRectangle(tiles[tile_num].x1, tiles[tile_num].y2 +1, tiles[tile_num].x2, tiles[tile_num].y2 + 2, YELLOW);
+           }
+           else if(tiles[tile_num].x1 == 61){
+               LCD_DrawFillRectangle(tiles[tile_num].x1, tiles[tile_num].y2 +1, tiles[tile_num].x2, tiles[tile_num].y2 + 2, BLUE);
+           }
+           else if(tiles[tile_num].x1 == 91){
+               LCD_DrawFillRectangle(tiles[tile_num].x1, tiles[tile_num].y2 +1, tiles[tile_num].x2, tiles[tile_num].y2 + 2, GREEN);
+           }
+           else if(tiles[tile_num].x1 == 121){
+               LCD_DrawFillRectangle(tiles[tile_num].x1, tiles[tile_num].y2 +1, tiles[tile_num].x2, tiles[tile_num].y2 + 2, CYAN);
+           }
+           else if(tiles[tile_num].x1 == 151){
+               LCD_DrawFillRectangle(tiles[tile_num].x1, tiles[tile_num].y2 +1, tiles[tile_num].x2, tiles[tile_num].y2 + 2, MAGENTA);
+           }
+           else if(tiles[tile_num].x1 == 181){
+               LCD_DrawFillRectangle(tiles[tile_num].x1, tiles[tile_num].y2 +1, tiles[tile_num].x2, tiles[tile_num].y2 + 2, BRRED);
+           }
+           else{
+               LCD_DrawFillRectangle(tiles[tile_num].x1, tiles[tile_num].y2 +1, tiles[tile_num].x2, tiles[tile_num].y2 + 2, BROWN);
+           }
     tiles[tile_num].y1 += 2;
     tiles[tile_num].y2 += 2;
     }
+
+    if (tiles[tile_num].y2 > 290){
+        LCD_DrawFillRectangle(tiles[tile_num].x1, 290, tiles[tile_num].x2, 290, WHITE);
+    }
+
+    if (tiles[tile_num].y2 >= 318){
+        LCD_DrawFillRectangle(tiles[tile_num].x1, 318, tiles[tile_num].x2, 318, WHITE);
+    }
+
 
     if (tiles[tile_num].y1 == 320){
         tiles[tile_num].y1 -= 340;
@@ -148,10 +108,10 @@ void move_tiles(int tile_num){
 
 }
 
-
-//
+//chekc tiles are in the range
 int check_led(int n){
-    if (tiles[n].y1 >160 && tiles[n].y2 < 320){
+    if (tiles[n].y2 >290 && tiles[n].y1 < 319){
+
         return 1;
     }
     else{
@@ -160,16 +120,8 @@ int check_led(int n){
 
 }
 
-
-
-
-
-
 void init_tiles(void){
     LCD_Clear(0);
-
-
-
     LCD_DrawFillRectangle(tiles[0].x1, tiles[0].y1, tiles[0].x2, tiles[0].y2, WHITE);
     LCD_DrawFillRectangle(tiles[1].x1, tiles[1].y1, tiles[1].x2, tiles[1].y2, WHITE);
     LCD_DrawFillRectangle(tiles[2].x1, tiles[2].y1, tiles[2].x2, tiles[2].y2, WHITE);
@@ -179,26 +131,3 @@ void init_tiles(void){
     LCD_DrawFillRectangle(tiles[6].x1, tiles[6].y1, tiles[6].x2, tiles[6].y2, WHITE);
     LCD_DrawFillRectangle(tiles[7].x1, tiles[7].y1, tiles[7].x2, tiles[7].y2, WHITE);
 }
-
-
-/*void basic_drawing(void)
-{
-    LCD_Clear(0);
-    for(;;)
-        LCD_DrawPicture(0,0,&load1);
-        LCD_DrawPicture(0,0,&load2);
-        if(start){
-            break;
-        }*/
-    /*LCD_Circle(50, 90, 40, 1, CYAN);
-    LCD_DrawTriangle(90,10, 120,10, 90,30, YELLOW);
-    LCD_DrawFillTriangle(90,90, 120,120, 90,120, GRAY);
-    LCD_DrawFillRectangle(10, 140, 120, 159, WHITE);
-    LCD_DrawString(20,141, BLACK, WHITE, "Test string!", 16, 0); // opaque background
-}*/
-
-//void bottom_line(void){
-//
-//    LCD_DrawLine(0,100,240,100,RED);
-//}
-
